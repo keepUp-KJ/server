79d1b29cedb2025d780782e7c47e49bdccb31ea5
kmagued <kmagued@gmail.com>
2020-10-18 12:31:50 +0200

    Added hash password function

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
 
-export const UserSchema = new Schema({
+const UserSchema = new Schema({
   email: {
     type: String,
     required: true,
@@ -12,3 +13,26 @@ export const UserSchema = new Schema({
     required: true,
   },
 });
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+mongoose.model("User", UserSchema);
