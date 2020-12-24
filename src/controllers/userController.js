460aba2655043c4aea5fb16963acb5efaa20b78c
kmagued <kmagued@gmail.com>
2020-12-24 07:27:56 +0200

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -93,9 +93,9 @@ exports.login = async (req, res) => {
 };
 
 exports.verifyEmail = async (req, res) => {
-  const { email, code } = req.body;
+  const { userId, code } = req.body;
 
-  User.findOne({ email }, function (err, user) {
+  User.findById({ userId }, function (err, user) {
     if (!user) {
       return res
         .status(406)
